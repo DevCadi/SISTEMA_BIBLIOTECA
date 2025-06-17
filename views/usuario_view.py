4fcd42a44afed6c7141e10872fc04c8aa4c0d3b1
Adriana Cordero Valencia <adrianacorderoval04@gmail.com>
2025-06-16 23:39:26 -0400

    Creación y modificación de la vista usuario

--- views/usuario_view.py
+++ views/usuario_view.py
@@ -1,1 +1,10 @@
 from flask import render_template
+
+def list(usuarios):
+    render_template('usuarios/index.html', usuarios=usuarios)
+
+def create():
+    return render_template('usuarios/create.html')
+
+def edit(usuario):
+    return render_template('usuarios/edit.html', usuario=usuario)
